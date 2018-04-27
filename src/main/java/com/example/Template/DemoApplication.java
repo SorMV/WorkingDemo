a716f86f82c2e0aa8d0fd0bce0f2fe14621e515f
sorokin.mv <cur2008@mail.ru>
2018-04-27 20:04:05 +0300

    html and php features

--- src/main/java/com/example/Template/DemoApplication.java
+++ src/main/java/com/example/Template/DemoApplication.java
@@ -1,5 +1,11 @@
 package com.example.Template;
 
+import com.vaadin.annotations.Theme;
+import com.vaadin.data.util.BeanItemContainer;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.Table;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -10,6 +16,8 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+
 import java.util.Arrays;
 import java.util.Collection;
 
@@ -28,23 +37,23 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
-
-@RestController
-class ReservationRestController {
-
-	private final ReservationRepository reservationRepository;
-
-    @Autowired
-	public ReservationRestController(ReservationRepository reservationRepository) {
-		this.reservationRepository = reservationRepository;
-	}
-
-	@RequestMapping (method = RequestMethod.GET, value = "/app")
-	Collection<Reservation> reservations(){
-		return this.reservationRepository.findAll();
-	}
-
-}
+//
+//@RestController
+//class ReservationRestController {
+//
+//	private final ReservationRepository reservationRepository;
+//
+//    @Autowired
+//	public ReservationRestController(ReservationRepository reservationRepository) {
+//		this.reservationRepository = reservationRepository;
+//	}
+//
+//	@RequestMapping (method = RequestMethod.GET, value = "/app")
+//	Collection<Reservation> reservations(){
+//		return this.reservationRepository.findAll();
+//	}
+//
+//}
 
 @Component
 class RepositoryCommandLineRunner implements CommandLineRunner {
@@ -66,10 +75,49 @@ class RepositoryCommandLineRunner implements CommandLineRunner {
 		this.reservationRepository = reservationRepository;
 	}
 }
-//@RepositoryRestResource
+
+@Controller
+class ReservationMvcController {
+	private final ReservationRepository reservationRepository;
+
+	@Autowired
+	public ReservationMvcController(ReservationRepository reservationRepository) {
+		this.reservationRepository = reservationRepository;
+	}
+
+	@RequestMapping("/reservations.php")
+	String page (Model model) {
+		model.addAttribute("reservations", this.reservationRepository.findAll());
+		return "reservations";
+	}
+}
+
+
+@SpringUI(path = "ui")
+@Theme("valo")
+class ReservationUI extends UI {
+	private final ReservationRepository reservationRepository;
+
+	@Autowired
+	public ReservationUI(ReservationRepository reservationRepository) {
+		this.reservationRepository = reservationRepository;
+	}
+
+	@Override
+	protected void init(VaadinRequest vaadinRequest) {
+        Table table=new Table();
+		table.setSizeFull();
+		table.setContainerDataSource(new BeanItemContainer<>(Reservation.class, this.reservationRepository.findAll()));
+		setContent(table);
+
+
+	}
+}
+
+@RepositoryRestResource
 interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
-//    @RestResource(path = "by-name")
+    @RestResource(path = "by-name")
 	Collection<Reservation> findByReservationName(@Param("rn") String rn);
 
 }
